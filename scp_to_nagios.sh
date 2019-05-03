3fdf1f3a9512c2da48718d01eabe686e9175c220
Dakoda <kodamondragon@gmail.com>
2019-05-02 20:15:14 -0700

    Update scp_to_nagios.sh

--- scp_to_nagios.sh
+++ scp_to_nagios.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bash generate_config.sh $1 $2
-gcloud compute scp $1.cfg koda@nagios4419:/etc/nagios/servers
+gcloud compute scp --zone us-east4-c $1.cfg koda@nagios4419:/etc/nagios/servers
 # Note: I had to add user nicolebade to group nagios using usermod -a -G nagios nicolebade
 # I also had to chmod 775 /etc/nagios/conf.d
 #usermod -a -G nagios nicolebade
-gcloud compute ssh koda@nagios4419 --command='sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg'
+gcloud compute ssh --zone us-east4-c koda@nagios4419 --command='sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg'
